01f0b2408b9f089f73b42f1ada21dfd9ebee1114
Shimwa Agape Valentin <savalentin75@gmail.com.com>
2023-10-16 23:52:20 +0200

    done

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker build -t sepsis-prediction-app .
-docker images
-docker run -p 8000:8000 --name sepsis-prediction-app image_id
-docker run -p 8000:8000 --name sepsis_tracker sepsis-prediction-app             
